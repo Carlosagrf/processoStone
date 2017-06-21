6e52dfd757e425d40c37ebec1cb9d1b8b3f11e0c
Carlosagrf <carlos.agrf@gmail.com>
2017-06-20 23:53:57 -0300

    Model e Controllers - Carro.

---
+++ RestAPI/RestAPI/Controllers/CarroController.cs
@@ -0,0 +1,76 @@
+﻿using RestAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestAPI.Controllers
+{
+    [RoutePrefix("api/carro")]
+    public class CarroController : ApiController
+    {
+        private static List<CarroModel> listaCarros = new List<CarroModel>();
+
+        [AcceptVerbs("POST")]
+        [Route("CadastraCarro")]
+        public string CadastrarUsuario(CarroModel usuario)
+        {
+            listaCarros.Add(usuario);
+
+            return "Usuário cadastrado com sucesso!";
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("AlterarCarro")]
+        public string AlterarUsuario(CarroModel carro)
+        {
+            listaCarros.Where(n => n.codigo == carro.codigo)
+                         .Select(s =>
+                         {
+                             s.codigo = carro.codigo;
+                             s.marca = carro.marca;
+                             s.modelo = carro.modelo;
+
+                             return s;
+
+                         }).ToList();
+
+
+
+            return "Usuário alterado com sucesso!";
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("ExcluirCarro/{codigo}")]
+        public string ExcluirUsuario(int codigo)
+        {
+            CarroModel carro = listaCarros.Where(n => n.codigo == codigo)
+                                                .Select(n => n)
+                                                .First();
+
+            listaCarros.Remove(carro);
+
+            return "Registro excluido com sucesso!";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarCarroPorCodigo/{codigo}")]
+        public CarroModel ConsultarUsuarioPorCodigo(int codigo)
+        {
+            CarroModel carro = listaCarros.Where(n => n.codigo == codigo)
+                                                .Select(n => n)
+                                                .FirstOrDefault();
+
+            return carro;
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarCarros")]
+        public List<CarroModel> ConsultarUsuarios()
+        {
+            return listaCarros;
+        }
+    }
+}
---
+++ RestAPI/RestAPI/Models/CarroModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestAPI.Models
+{
+    public class CarroModel
+    {
+        public CarroModel()
+        {
+        }
+
+        public CarroModel(int codigo, string marca, string modelo)
+        {
+            this.codigo = codigo;
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+
+        public int codigo;
+
+        public string marca;
+
+        public string modelo;
+
+    }
+}
